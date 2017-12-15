0e5409bbc08b801ca50c64886cab0cdae6d3962e
cassidylaidlaw <cassidy256@gmail.com>
2017-12-15 13:17:27 -0500

    Fixed or-0 test so it works with the testing framework

--- tests/or-0.scm
+++ tests/or-0.scm
@@ -1,2 +1,2 @@
-(list (or) (or '1) (let* ([x '1] [y (or '#f (set! x (+ x '1)))]) (cons y x))
-  (let* ([x '1] [y (or '#t (set! x (+ x '1)))]) (cons y x)))
+(list (or) (or 1) (let* ([x 1] [y (or #f (set! x (+ x 1)))]) (cons (void? y) x))
+  (let* ([x 1] [y (or #t (set! x (+ x 1)))]) (cons y x)))
